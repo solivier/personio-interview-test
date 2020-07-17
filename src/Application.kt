6672bfcae151225d774ed36700a5e5b28cdfdb34
Olivier Soulet <olivier.soulet@akeneo.com>
2020-07-18 01:08:22 +0200

    add http management

--- src/Application.kt
+++ src/Application.kt
@@ -8,6 +8,13 @@ import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.auth.*
 import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.personio.Application.AdjacencyListBuilder
+import com.personio.Application.HierarchyTreeBuilder
+import com.personio.Application.HierarchyTreeValidator
+import com.personio.Domain.Employee
+import com.personio.Infrastructure.InMemoryEmployeeRepository
 import io.ktor.jackson.*
 import io.ktor.client.*
 
@@ -44,19 +51,67 @@ fun Application.module(testing: Boolean = false) {
     }
 
     routing {
+        val repository = InMemoryEmployeeRepository()
+
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            call.respondText("HELLO WORLD!", contentType = ContentType.Application.Json)
         }
 
-        authenticate("myBasicAuth") {
-            get("/protected/route/basic") {
-                val principal = call.principal<UserIdPrincipal>()!!
-                call.respondText("Hello ${principal.name}")
+        post("/newHierarchy") {
+            repository.empty();
+            val post = call.receiveText()
+
+            val mapper = jacksonObjectMapper();
+
+            val employeeHierarchy = mapper
+                    .readTree(post)
+                    .deepCopy<ObjectNode>()
+                    .fields()
+                    .asSequence()
+                    .map { Pair(it.key, it.value.asText()!!) }
+                    .toList()
+
+            val mapToAdjacencyList = AdjacencyListBuilder();
+
+            val adjacencyList = mapToAdjacencyList.toAdjacencyList(employeeHierarchy).map { it.first to it.second }
+
+            val validator = HierarchyTreeValidator()
+            validator.validate(adjacencyList)
+
+            val employees = employeeHierarchy.flatMap { listOf(it.first, it.second) }.toSet().map { it to Employee(it) }
+
+            val adjacencyListAsMap = adjacencyList.toMap()
+            val employeesAsMap = employees.toMap()
+
+            employees.forEach { employee ->
+                adjacencyListAsMap[employee.first]!!.forEach { managed -> employee.second.supervise(employeesAsMap[managed]!!) }
             }
+
+            employees.forEach {
+                repository.save(it.second)
+            }
+
+            val getTreeHierarchy = HierarchyTreeBuilder(repository)
+
+            val json = getTreeHierarchy.invoke().toJson()
+
+            call.respond(json)
         }
 
-        get("/json/jackson") {
-            call.respond(mapOf("hello" to "world"))
+        get("/employeeHierarchy") {
+            val getTreeHierarchy = HierarchyTreeBuilder(repository)
+            val employeeNode = getTreeHierarchy.invoke()
+
+            call.respondText(employeeNode.toJson(), contentType = ContentType.Application.Json)
+        }
+
+        get("/employeeSupervisors/{name}") {
+            val name = call.parameters["name"] ?: throw RuntimeException("Name is empty")
+
+            val getTreeHierarchy = HierarchyTreeBuilder(repository)
+            val actual = getTreeHierarchy.getSupervisors(name)
+
+            call.respondText(actual.toJson(), contentType = ContentType.Application.Json)
         }
     }
 }
