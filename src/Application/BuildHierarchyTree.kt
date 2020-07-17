c11a38da707b4087f6edc0127aedc5a590751633
Olivier Soulet <olivier.soulet@akeneo.com>
2020-07-17 20:13:37 +0200

    add possibility to get supervisors

--- src/Application/BuildHierarchyTree.kt
+++ src/Application/BuildHierarchyTree.kt
@@ -9,4 +9,23 @@ class BuildHierarchyTree(val repository: EmployeeRepository) {
 
         return root!!.generateGraph(repository.all().filter { it.name != root.name })
     }
+
+    fun getSupervisors(name: String): Graph {
+        val leaf = repository.all().findLast { e -> e.name == name }
+        val leafNode = Graph(leaf!!.name)
+
+        if (null == leaf.isDirectlyManagedBy) {
+            return leafNode
+        }
+
+        val supervisorOne = repository.all().findLast { it.name == leaf.isDirectlyManagedBy }
+
+        if (supervisorOne!!.isDirectlyManagedBy == null) {
+            return Graph(supervisorOne.name, listOf(leafNode))
+        }
+
+        val supervisorTwo = repository.all().findLast { it.name == supervisorOne.isDirectlyManagedBy }
+
+        return Graph(supervisorTwo!!.name, listOf(Graph(supervisorOne.name, listOf(leafNode))))
+    }
 }
--- test/Application/BuildHierarchyTreeTest.kt
+++ test/Application/BuildHierarchyTreeTest.kt
@@ -49,4 +49,40 @@ class BuildHierarchyTreeTest {
 
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun it_returns_supervisors() {
+        val nick = Employee("Nick")
+        val barbara = Employee("Barbara")
+        val pete = Employee("Pete")
+        val sophie = Employee("Sophie")
+        val jonas = Employee("Jonas")
+
+        nick.supervise(barbara)
+        nick.supervise(pete)
+        sophie.supervise(nick)
+        jonas.supervise(sophie)
+
+        val repository = InMemoryEmployeeRepository()
+
+        val employeesList = listOf(nick, barbara, pete, sophie, jonas)
+        employeesList.forEach { repository.save(it) }
+
+        val getTreeHierarchy = BuildHierarchyTree(repository)
+        val actual = getTreeHierarchy.getSupervisors("Barbara")
+
+        val expected = Graph(
+            "Sophie",
+            listOf(
+                Graph(
+                    "Nick",
+                    listOf(
+                        Graph("Barbara")
+                    )
+                )
+            )
+        )
+
+        assertEquals(expected, actual)
+    }
 }
